5c34f5001c9fdf701860a425e378a9f09ceebf67
Nathan Administrador <natrontrejo@gmail.com>
2023-01-23 00:39:13 -0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>welcome</title>
+
+
+<body>
+  
+  <h1 class="center"> <span class="primary">DEL</span> <span class="danger">TA</span></h1>
+  
+  
+  <form action="{{ route('dashboard') }}" method="get">
+  <div style="text-align: center;">
+  <button class="button button1">INICIA SESIÓN</button>
+  </div>
+  </form>
+
+</body>
+
+</html>
